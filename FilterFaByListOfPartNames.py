41d19cdf7300ad90fa3e569a94a0c859a16c81cd
PotapenkoEugene <g.r.e.e.d@yandex.ru>
2021-12-04 21:19:13 +0200

    fix bugs with several partnames

--- FilterFaByListOfPartNames.py
+++ FilterFaByListOfPartNames.py
@@ -1,11 +1,43 @@
 import sys
-
+from collections import defaultdict
 ##################################
 fasta_filepath = sys.argv[1]
-partname = sys.argv[2].strip().split(',')  # names separated by comma
+partname = sys.argv[2].strip()  # names separated by comma
+# for el1 OR el2 use brackets: el1, [el2,el3]
 ##################################
 
-outfile = '.'.join(fasta_filepath.split('.')[:-1]) + '_' + '-'.join(partname) + '.fa'
+def parse_partnames(namelist):
+    partnames = defaultdict(list)
+    elnum = 1
+    OR = False
+    curel = []
+    for el in namelist:
+
+        if el not in [',', '[', ']']:
+            curel.append(el)
+
+        elif el == ',' and not OR:
+            partnames[elnum].append(''.join(curel))
+            elnum += 1
+            curel = []
+
+        elif el == ',' and OR:
+            partnames[elnum].append(''.join(curel))
+            curel = []
+
+        elif el == '[':
+            OR = True
+
+        elif el == ']':
+            OR = False
+    partnames[elnum].append(''.join(curel))
+
+    return partnames
+
+namedict = parse_partnames(partname)
+
+outpartnames = '_'.join(sum([i for i in namedict.values()], []))
+outfile = '.'.join(fasta_filepath.split('.')[:-1]) + '_' + outpartnames + '.fa'
 
 with open(fasta_filepath) as fasta_file, \
      open(outfile, 'w') as w:
@@ -17,7 +49,10 @@ with open(fasta_filepath) as fasta_file, \
         if line.startswith('>'):
 
             print(f'INFO:\tSearch in {line}')
-            check = sum([1 for i in partname if i in line]) == len(partname)
+            print(namedict)
+            matches = len([True for i in namedict if [True for j in namedict[i] if j in line]])
+            print(matches)
+            check = sum([True for i in namedict if [True for j in namedict[i] if j in line]]) == len(namedict)
 
             if check:
                 print(f'INFO:\tSuccess')
