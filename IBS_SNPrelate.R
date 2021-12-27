d043ab2f0a02eca42c434ddc8c3b9f37550c0564
PotapenkoEugene <g.r.e.e.d@yandex.ru>
2021-12-27 11:18:34 +0200

    base edit

--- IBS_SNPrelate.R
+++ IBS_SNPrelate.R
@@ -7,16 +7,11 @@ library(SNPRelate)
 
 snpgdsVCF2GDS(In, paste(Tag,".gds",sep=""))
 GDS <- paste(Tag,".gds",sep="")
-organGDS <- snpgdsOpen(GDS)
-organ.PCA<-snpgdsPCA(organGDS, autosome.only=FALSE, num.thread = 32)
+oGDS <- snpgdsOpen(GDS)
 
-organPCA<-data.frame(sample.id=organ.PCA$sample.id, EV1=organ.PCA$eigenvect[,1], EV2=organ.PCA$eigenvect[,2],EV3=organ.PCA$eigenvect[,3],EV4=organ.PCA$eigenvect[,4],EV5=organ.PCA$eigenvect[,5],stringsAsFactors=FALSE)
+# perform identity-by-state calculations
+ibs <- snpgdsIBS(oGDS)
+write.tsv(ibs$ibs, paste0(Tag,'_IBS.tsv'))
 
-print(organ.PCA$varprop[1:5] * 100)
-
-write.table(organPCA, 
-	    paste(Tag,".PCA.txt", sep=""),
-	    col.names = T,
-            quote = FALSE, 
-	    row.names = FALSE, 
-	    sep="\t")
+# close the file
+snpgdsClose(oGDS)
