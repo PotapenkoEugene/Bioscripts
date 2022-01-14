f3c9d87f1dcd17fa79e028290b1c56af7e278bf8
PotapenkoEugene <g.r.e.e.d@yandex.ru>
2022-01-14 18:09:09 +0200

    Add Pseudo param

--- FaPairwiseDist.R
+++ FaPairwiseDist.R
@@ -3,11 +3,11 @@ r["CRAN"] = "http://cran.us.r-project.org"
 options(repos = r)
 ############################################################################
 if (!requireNamespace("seqinr", quietly = TRUE)) install.packages("seqinr")
-if (!requireNamespace("ggplot2", quietly = TRUE)) install.packages("ggplot2")
-
 library(seqinr)
+if (!requireNamespace("ggplot2", quietly = TRUE)) install.packages("ggplot2")
 library(ggplot2)
-# #############################################################################
+############################################################################
+
 args = commandArgs(trailingOnly=TRUE)
 # test if there is at least one argument: if not, return an error
 if (length(args)==0) {
@@ -17,6 +17,7 @@ if (length(args)==0) {
   args[2] = "out"
 }
 
+# Args
 path = args[1]
 outpath = args[2]
 
@@ -27,6 +28,7 @@ mat <- dist.alignment(myseqs, matrix = "identity")
 out = paste0(outpath, '_DistMatrix.tsv')
 write.table(as.matrix(mat), out, quote = FALSE, sep='\t')
 #############################################################################
+
 # Make PcoA from dist matrix
 # Use Stat Quest pipeline: https://www.youtube.com/watch?v=pGAUHhLYp5Q
 mds <- cmdscale(mat, eig=T, x.ret=T)
@@ -37,18 +39,16 @@ mds.data = data.frame(Sample = rownames(mds.values),
                       X = mds.values[,1], # PC1 and PC2
                       Y = mds.values[,2])
 
-# Make graph 
-spec = sub('(.*)-[0-9]+.*', '\\1', mds.data$Sample)
-tk = sub('.*-([0-9]+)_K.*', '\\1', mds.data$Sample)
-kinpos = sub(".*_(K[12]){1}.*", "\\1", mds.data$Sample)
-mds.data$Species = spec
-mds.data$TK = tk
-mds.data$KinPos = kinpos
+# Make graph  (I'm a GOD of regex!)
+mds.data$Species = sub('(.*)-[0-9]+.*', '\\1', mds.data$Sample)
+mds.data$TK = sub('.*-([0-9]+)_K.*', '\\1', mds.data$Sample)
+mds.data$KinPos = sub(".*_(K[12]){1}.*", "\\1", mds.data$Sample)
+mds.data$Pseudo = sub('[^_]*_[^_]*_[^_]*_[^_]*_[^_]*_[^_]*_([A-Z]*)', '\\1', mds.data$Sample)
 
 out = paste0(outpath, '_MDSplor.png')
 png(file=out, width = 2400, height = 1200)
 
 ggplot(mds.data) +
-  geom_point(aes(X,Y,shape = KinPos), size = 5)
+  geom_point(aes(X,Y,shape = KinPos, color = Pseudo), size = 5)
 
 dev.off()
