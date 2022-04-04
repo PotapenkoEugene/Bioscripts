11d5178521b409e4bd9b9bc927d24b0dd09c3bf2
PotapenkoEugene <g.r.e.e.d@yandex.ru>
2022-04-04 11:41:45 +0300

    Add mkdir BarSplit

--- BarcodeProcessingAndTrimming.sh
+++ BarcodeProcessingAndTrimming.sh
@@ -12,23 +12,26 @@ CPU=$5
 
 R1name=${R1%%.*}
 R2name=${R2%%.*}
+
+R1trim=${R1name}.trim.fastq.gz
+R2trim=${R2name}.trim.fastq.gz
 mkdir QC trim
 
 # QC raw reads
 #fastqc $R1 $R2 -t $CPU -o QC
 
 # Trim first NNN
-if test -f "trim/${R1name}.trim.fastq.gz"; then
-	echo "File trim/${R1name}.trim.fastq.gz already exist - SKIP TRIMMING"
+if test -f "trim/${R1trim}"; then
+	echo "File trim/${R1trim} already exist - SKIP TRIMMING"
 else
-	fastp -w $CPU -f 3 -i $R1 -I $R2 -o trim/${R1name}.trim.fastq.gz -O trim/${R2name}.trim.fastq.gz
+	fastp -w $CPU -f 3 -i $R1 -I $R2 -o trim/${R1trim} -O trim/${R2trim}
 fi
 
 cd trim
-R1=${R1name}.trim.fastq.gz
-R2=${R2name}.trim.fastq.gz
 
 # BarcodeSplit
-zcat $R1 | BarcodeSplitter --bcfile ../${BARCODES} --bol --mismatch $MISMATCH --prefix BarSplitR1/ --suffix "_R1.fastq"
-zcat $R2 | BarcodeSplitter --bcfile ../${BARCODES} --bol --mismatch $MISMATCH --prefix BarSplitR2/ --suffix "_R2.fastq"
+mkdir BarSplitR1 BarSplitR2
+
+(echo "zcat $R1trim | BarcodeSplitter --bcfile ../${BARCODES} --bol --mismatches $MISMATCH --prefix BarSplitR1/ --suffix "_R1.fastq" --debug"
+ echo "zcat $R2trim | BarcodeSplitter --bcfile ../${BARCODES} --bol --mismatches $MISMATCH --prefix BarSplitR2/ --suffix "_R2.fastq" --debug") | parallel
 
