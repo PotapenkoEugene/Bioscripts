afa7d3e14983f4c4a77d2f1f5b8b1afc16a9fea1
PotapenkoEugene <g.r.e.e.d@yandex.ru>
2022-04-26 21:51:04 +0300

    smll changes

--- processVcf.sh
+++ processVcf.sh
@@ -9,6 +9,7 @@ STEPSIZE=$4 # 10 (kb)
 MAF=$5 # 0.01
 NE=20000000 # effective population size
 MEM=$6
+CPU=$7
 source ${CONDA_PREFIX}/etc/profile.d/conda.sh
 ################################
 
@@ -17,11 +18,11 @@ JAVA_TOOL_OPTIONS="-Xmx${MEM}g"
 
 conda activate beagle
 # impute vcf with beagle 5.2
-beagle gt=${VCFGZ} out=${NAME}.beagle ne=${NE}
-beagle gt=${NAME}.beagle.vcf.gz out=${NAME}.beagle2 ne=${NE}
+beagle gt=${VCFGZ} out=${NAME}.beagle ne=${NE} nthreads=${CPU}
+beagle gt=${NAME}.beagle.vcf out=${NAME}.beagle2 ne=${NE} nthreads=${CPU}
 
-VCFGZIMP=${NAME}.beagle2.vcf.gz
-NAMEIMP=$(basename ${VCFGZIMP} .vcf.gz)
+VCFGZIMP=${NAME}.beagle2.vcf
+NAMEIMP=$(basename ${VCFGZIMP} .vcf)
 #prune vcf
 plink --vcf ${VCFGZIMP} --double-id --allow-extra-chr --set-missing-var-ids @:# --indep-pairwise ${WINSIZE} ${STEPSIZE} ${LD} --out ${NAMEIMP}.LD${LD}
 sed 's/:/ /g' ${NAMEIMP}.LD${LD}.prune.in > ${NAMEIMP}.LD${LD}.list
