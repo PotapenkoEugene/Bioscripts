b7fbb4034f305db507ea2c1a13666f33cf825955
PotapenkoEugene <g.r.e.e.d@yandex.ru>
2022-11-03 15:12:22 +0200

    samples

--- download_ncbi_GSM.sh
+++ download_ncbi_GSM.sh
@@ -3,4 +3,4 @@
 GSM=$1 
 GSMn="$(echo ${GSM} | cut -c 1-$((`echo $GSM | wc -m` - 4)))nnn"
 
-wget https://ftp.ncbi.nlm.nih.gov/geo/series/${GSMn}/${GSM}/suppl/${GSM}*
+wget https://ftp.ncbi.nlm.nih.gov/geo/samples/${GSMn}/${GSM}/suppl/${GSM}*
