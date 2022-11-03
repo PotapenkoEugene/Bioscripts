14ece2aae9088eca2a46c991b301f353b8943d00
PotapenkoEugene <g.r.e.e.d@yandex.ru>
2022-11-03 13:56:16 +0200

    change path

--- download_ncbi_GSE.sh
+++ download_ncbi_GSE.sh
@@ -3,4 +3,4 @@
 GEO=$1 
 GEOn="$(echo ${GEO} | cut -c 1-$((`echo $GEO | wc -m` - 4)))nnn"
 
-wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/${GEOn}/${GEO}/suppl/${GEO}_*
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/${GEOn}/${GEO}/suppl/${GEO}_RAW.tar
