a18832da7b19e8c9550028b2f0f7f7d3496f3961
PotapenkoEugene <g.r.e.e.d@yandex.ru>
2022-11-09 18:57:13 +0200

    init

---
+++ HeatMapFromPeaksAndBW.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# deepTools needed
+
+PEAKS=$1 # one file
+SIGNAL=$2 # one or many(sep = ' ')
+LABELS=$3 # one or many(sep = ' ')
+BEFORE=$4 #bp before left end of the region
+AFTER=$5 # bp after right end of the region
+SUFFIX=$6
+CPU=$7
+
+computeMatrix scale-regions -S $SIGNAL -R $PEAKS -b 3000 -a 3000 -o ComputeMatrix_${SUFFIX}.gz -p $CPU --samplesLabel $LABELS
+
+plotHeatmap -m ComputeMatrix_${SUFFIX}.gz -out ComputeMatrix_${SUFFIX}.png --whatToShow 'heatmap and colorbar' --dpi 300 --boxAroundHeatmaps no --colorList 'white,blue' --interpolationMethod nearest --missingDataColor white --startLabel S --endLabel E -min 0 -max 20
