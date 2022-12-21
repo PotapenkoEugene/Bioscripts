3f62b1870b554ab8b40c4fc11b46e2192d150106
PotapenkoEugene <g.r.e.e.d@yandex.ru>
2022-12-21 20:43:22 +0200

    Add stdin option

--- AddSuffixToDuplicatedRows.sh
+++ AddSuffixToDuplicatedRows.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-file=$1 # text/table file
+file=${1:-/dev/stdin} # text/table file
 awk 'NR==FNR && index($0, ">")==1 { a[$0]++ } NR!=FNR && a[$0]>1 { $0=$0"_"(++b[$0]) } NR!=FNR' $file $file
