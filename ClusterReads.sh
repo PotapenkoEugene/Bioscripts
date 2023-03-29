6ec78ce2e16bbf58f012e30eb3843ad57ecc6223
PotapenkoEugene <g.r.e.e.d@yandex.ru>
2023-03-29 18:18:26 +0300

    init

---
+++ ClusterReads.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+FASTQ=$1
+CPU=$2
+IDENTITY=$3
+NTOP=$4
+
+# 2fasta
+fasta=$(basename $FASTQ .fastq).fasta
+seqtk seq -a $FASTQ > $fasta 
+
+# Rename
+fastaRE=$(basename $fasta .fasta).RE.fasta
+cat $fasta | awk '/^>/{print ">" ++i; next}{print}' > $fastaRE
+
+# Clustering
+clusters=$(basename $fastaRE .fasta).clstr
+cd-hit-est -T $CPU -M 0 -i $fastaRE -o $(basename $fastaRE .fasta) -c $IDENTITY
+
+# Retrieve sequences by clusters
+## Rename cluster file
+sed -i $clusters 's/Cluster /Cluster_/'
+## Make list of read names for each cluster and retrieve sequences
+for i in `seq 0 $(($NTOP-1))`
+	do
+	echo "Cluster_${i}" >> Cluster.list
+	faSomeRecords $clusters Cluster.list Cluster.${i}
+	cat Cluster.${i} | tail -n +2 | cut -f2 | cut -f2 -d ' ' | sed 's/>//' > Cluster.${i}.list
+	faSomeRecords $fastaRE Cluster.${i}.list Cluster.${i}.fasta
+	done
