31ae757291a121d16ffb27ba2b6f574cb34b02cd
PotapenkoEugene <g.r.e.e.d@yandex.ru>
2024-05-20 10:36:33 +0300

    fix

--- sortH.sh
+++ sortH.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-FILE=$1
+FILE=$1 #tsv file
 COLUMN=$2
 NUMERIC=$3 
 
 if [ "${NUMERIC}" = "n" ]; then
-  (head -n 1 ${file} && tail -n +2 ${file} | sort -t $'\t' -nk${COLUMN},${COLUMN})
+	( head -n 1 ${FILE} && tail -n +2 ${FILE} ) | sort -t $'\t' -nk ${COLUMN},${COLUMN}
 else
-  (head -n 1 ${file} && tail -n +2 ${file} | sort -t $'\t' -k${COLUMN},${COLUMN})
+	( head -n 1 ${FILE} && tail -n +2 ${FILE} ) | sort -t $'\t' -k ${COLUMN},${COLUMN}
 fi
 
