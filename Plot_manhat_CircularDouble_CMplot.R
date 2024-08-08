db64a87a6144838eaa21e9b4d9409dbafe6dca2e
PotapenkoEugene <g.r.e.e.d@yandex.ru>
2024-08-08 11:19:57 +0300

    update

--- Plot_manhat_CircularDouble_CMplot.R
+++ Plot_manhat_CircularDouble_CMplot.R
@@ -7,6 +7,9 @@ args = commandArgs(trailingOnly=TRUE)
 GWAS1 = args[1] # LFMM gwasUniq custom format
 GWAS2 = args[2] # EMMAX gwasUniq custom format
 TRAIT = args[3]
+if(length(args) == 4){
+	CUSTOM_SNP = fread(args[4], header = F)$V1 # should be list without header 
+}else(CUSTOM_SNP = NULL)
 ################################
 
 
@@ -19,33 +22,69 @@ lfmm.res = fread(GWAS1, sep = '\t', header = T) %>%
 message('INFO: PREPARING GWAS2 DATA FRAME')
 emmax.res = fread(GWAS2, sep = '\t', header = T)
 
-lfmm.res$EMMAX = emmax.res$pvalue
+lfmm.res$EMMAX = emmax.res$pvalue # Combine pvalues
 
-pval.threshold = 0.05 / nrow(lfmm.res)
+if(is.null(CUSTOM_SNP)){
+	pval.threshold = 0.05 / nrow(lfmm.res)
 
-# Plot
-message('INFO: PLOTTING')
-CMplot(lfmm.res  %>% as.data.frame, # return some error to data.table 
-       type="p",
-       plot.type="c",
-       r=0.4,
-       col=c("dodgerblue4", "deepskyblue"),
-       cex = 0.2,
-       signal.cex = 0.4,
+	# Plot
+	message('INFO: PLOTTING WITH PVALUE THRESHOLD')
+	CMplot(lfmm.res  %>% as.data.frame, # return some error to data.table 
+	       type="p",
+	       plot.type="c",
+	       r=0.4,
+	       col=c("dodgerblue4", "deepskyblue"),
+	       cex = 0.2,
+	       signal.cex = 0.4,
+	
+	       threshold= pval.threshold,
+	       cir.chr.h=1.5,
+	       amplify=T,
+	       threshold.lty=1,
+	       threshold.col="black",
+	       signal.line=1,
+	       signal.col="red",
+	       chr.den.col=c("darkgreen","yellow","red"),
+	       bin.size=1e6,
+	       outward=FALSE,
+	       file.name = paste0('CMplot_CircularDouble_EMMAXandLFMM_', TRAIT, '.png'),
+	       file="png",
+	       dpi=1200,
+	       file.output=TRUE,
+	       verbose=TRUE,
+	       width=10,height=10)
+ }else{
 
-       threshold= pval.threshold,
-       cir.chr.h=1.5,
-       amplify=T,
-       threshold.lty=1,
-       threshold.col="black",
-       signal.line=1,
-       signal.col="red",
-       chr.den.col=c("darkgreen","yellow","red"),
-       bin.size=1e6,
-       outward=FALSE,
-       file.name = paste0('CMplot_CircularDouble_EMMAXandLFMM_', TRAIT, '.png'),
-       file="jpg",
-       dpi=1200,
-       file.output=TRUE,
-       verbose=TRUE,
-       width=10,height=10)
+	 # Plot
+        message('INFO: PLOTTING WITH CUSTOM SNP HIGHLIGHTER')
+        CMplot(lfmm.res  %>% as.data.frame, # return some error to data.table
+               type="p",
+               plot.type="c",
+               r=0.4,
+               col=c("dodgerblue4", "deepskyblue"),
+               cex = 0.2,
+               signal.cex = 0.4,
+
+               #threshold= pval.threshold,
+		highlight = CUSTOM_SNP,
+	       	highlight.cex = 0.5,
+		highlight.col = 'red',
+
+
+               cir.chr.h=1.5,
+               amplify=T,
+               threshold.lty=1,
+               threshold.col="black",
+               signal.line=1,
+               signal.col="red",
+               chr.den.col=c("darkgreen","yellow","red"),
+               bin.size=1e6,
+               outward=FALSE,
+               file.name = paste0('CMplot_CircularDouble_EMMAXandLFMM_customSNP_', TRAIT, '.png'),
+	       file="png",
+               dpi=1200,
+               file.output=TRUE,
+               verbose=TRUE,
+               width=10,height=10)
+}
+
