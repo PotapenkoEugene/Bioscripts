4da075a261a36067b585be15fb914fd3ceb8abb4
PotapenkoEugene <g.r.e.e.d@yandex.ru>
2025-12-14 18:41:15 +0200

    init

--- Run_LDBlockShow.sh
+++ Run_LDBlockShow.sh
@@ -11,6 +11,6 @@ outprefix=$8
 density=300 # svg2png
 scale=0.4 # svg2png 
 
-LDBlockShow -InVCF $vcf -SubPop ${subpops} -InGWAS $gwasresult -InGFF $gff -SeleVar ${selevar} -NoShowLDist 9000000 -BlockType 1 -Region $position -OutPut ${outprefix} -NoGeneName -Cutline $cutline
+LDBlockShow -InVCF $vcf -SubPop ${subpops} -InGWAS $gwasresult -InGFF $gff -SeleVar ${selevar} -NoShowLDist 35000000 -BlockType 1 -Region $position -OutPut ${outprefix} -NoGeneName -Cutline $cutline
 cairosvg -d $density -f png -o ${outprefix}.png ${outprefix}.svg
 
