7cc5e6a2a82a330e3edc5327a13dbc8f6baad7bc
PotapenkoEugene <g.r.e.e.d@yandex.ru>
2026-01-27 15:31:06 +0200

    add specification of ssh key to use

--- TransferFromMigal.sh
+++ TransferFromMigal.sh
@@ -3,4 +3,4 @@
 MIGALPATH=$1
 LOCALPATH=$2
 
-scp -r potapgene@172.16.11.55:${MIGALPATH} $LOCALPATH
+scp -o IdentitiesOnly=yes -i ~/.ssh/id_rsa -r potapgene@172.16.11.55:${MIGALPATH} $LOCALPATH
--- TransferToMigal.sh
+++ TransferToMigal.sh
@@ -3,4 +3,4 @@
 MIGALPATH=$1
 LOCALPATH=$2
 
-scp -r $LOCALPATH potapgene@172.16.11.55:${MIGALPATH}
+scp -o IdentitiesOnly=yes -i ~/.ssh/id_rsa -r $LOCALPATH potapgene@172.16.11.55:${MIGALPATH}
